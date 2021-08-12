00ec184efba88291f23ddb26aee1f687dd2d1ec5
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-08-12 09:51:25 +0300

    Renkler route yapıldı ve html de liste olarak getirildi.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarComponent } from './component/car/car.component';
+import { ColorComponent } from './component/color/color.component';
 import { MenuComponent } from './component/menu/menu.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full",component:MenuComponent},
-  {path:"cars/getcardetailsdto",component:CarComponent}
+  {path:"cars/getcardetailsdto",component:CarComponent},
+  {path:"colors/getallcolors",component:ColorComponent}
 ];
 
 @NgModule({
--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Color } from '../models/color';
+import { ColorService } from '../services/color.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  colors:Color[]=[]
+  constructor(private colorService:ColorService) { }
 
   ngOnInit(): void {
+  }
+
+  getAllColorComponent(){
+    this.colorService.getAllColors().subscribe(response=>{
+      this.colors=response.data;
+    });
   }
 
 }
