d83f4fee47e4f84cb950b42b8db0333707366006
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-08-12 10:11:10 +0300

    Menüden  Brand Routing işlemi yapıldı.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandComponent } from './component/brand/brand.component';
 import { CarComponent } from './component/car/car.component';
 import { ColorComponent } from './component/color/color.component';
 import { MenuComponent } from './component/menu/menu.component';
@@ -7,7 +8,8 @@ import { MenuComponent } from './component/menu/menu.component';
 const routes: Routes = [
   {path:"",pathMatch:"full",component:MenuComponent},
   {path:"cars/getcardetailsdto",component:CarComponent},
-  {path:"colors/getallcolors",component:ColorComponent}
+  {path:"colors/getallcolors",component:ColorComponent},
+  {path:"brands/getallbrands",component:BrandComponent}
 ];
 
 @NgModule({
