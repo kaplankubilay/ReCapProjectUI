af78f238722f1b7fdd1de17116e9f708e4582516
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-09-08 21:09:11 +0300

    fix

--- src/app/component/car/car.component.ts
+++ src/app/component/car/car.component.ts
@@ -1,8 +1,11 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { observable } from 'rxjs';
 import { CarDetail } from '../models/carDetail';
+import { CarImage } from '../models/carImage';
 import { CarService } from '../services/car.service';
+import { CarImageService } from '../services/carImage.service';
 
 @Component({
   selector: 'app-car',
@@ -15,16 +18,22 @@ export class CarComponent implements OnInit {
 
   isLoaded:boolean=false;
 
+  // carImages:CarImage[];
+
+  // carImage:CarImage;
+
+  // basePath ="https://localhost:44319/";
+
   constructor(private carService:CarService,
               private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
       if(params["brandId"]){
-        this.getCarsByBrandIdMetod(params["brandId"])
+        this.getCarsByBrandIdMetod(params["brandId"]);
       }
       else if(params["colorId"]){
-        this.getCarsByColorIdMetod(params["colorId"])
+        this.getCarsByColorIdMetod(params["colorId"]);
       }
       else{
         this.getCarDetailMetod();
@@ -52,5 +61,30 @@ export class CarComponent implements OnInit {
       this.isLoaded=true;
     })
   }
+
+  // getCarImageByCarId(carId:number) : CarImage[]{
+  //   this.carImageService.getCarImagesMetod(carId).subscribe(response=>{
+  //     this.carImages=response.data;
+  //   })
+  //   return this.carImages;
+  // }
+
+  // getFirstImageByCarId(carId:number):CarImage{
+  //     this.carImage =  this.getCarImageByCarId(carId)[0];
+  //     return this.carImage;
+  // }
+
+  // getCurrentImageClass(image:CarImage){
+  //   if(this.carImages[0]==image){
+  //     return "carousel-item active";
+  //   } else {
+  //     return "carousel-item ";
+  //   }
+  // }
+
+  // getPath(){
+  //   let postPath="https://localhost:44319/";
+  //   return postPath +this.carImage.imagePath;
+  // }
 
 }
