8d998de566a672c05b24995919e95da745d4976d
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-09-08 21:10:20 +0300

    Cardetailmodel duzenlendi.

--- src/app/component/models/carDetail.ts
+++ src/app/component/models/carDetail.ts
@@ -3,9 +3,11 @@ export interface CarDetail{
     colorId:number;
     brandId:number;
     brandName:string;
+    modelName:string;
     fuelName:string;
     colorName:string;
     description:string;
     modelYear?:number;
     plate:string
+    imagePaths:string[];
 }
